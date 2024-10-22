72ea467a1bff12e375b40280d235f09b6556b327
LordTran93 <tran42@seznam.cz>
2024-10-22 12:51:12 +0200

    Remove vypocet button and add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a simple net wage calculator
+This was done by using Streamlit and pandas frameworks. Application can be accesed by link below:
+https://wage-calculator-lordtran93.streamlit.app/
--- main.py
+++ main.py
@@ -22,84 +22,83 @@ elif (pocet_deti ==2):
     deti_sleva = deti1 + deti2
 elif (pocet_deti >=3):
     deti_sleva = deti1 + deti2 + ((pocet_deti - 2) * deti3)
-if st.button("Výpočet"):
-    st.write("## Čistá mzda")
-    st.write("### Zaměstnanec")
-    cista_mzda =  hruba_mzda - zamestnanec_socialni - zamestnanec_zdravotni -  dan + dan_sleva + deti_sleva
+st.write("## Čistá mzda")
+st.write("### Zaměstnanec")
+cista_mzda =  hruba_mzda - zamestnanec_socialni - zamestnanec_zdravotni -  dan + dan_sleva + deti_sleva
 
 
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric(label="Čistá mzda", value=f"{cista_mzda} Kč")
-    col2.metric(label="Zaměstnanec sociální", value=f"{zamestnanec_socialni} Kč")
-    col3.metric(label="Zaměstnanec zdravotní", value=f"{zamestnanec_zdravotni} Kč")
-    col4.metric(label="Daň", value=f"{dan} Kč")
+col1, col2, col3, col4 = st.columns(4)
+col1.metric(label="Čistá mzda", value=f"{cista_mzda} Kč")
+col2.metric(label="Zaměstnanec sociální", value=f"{zamestnanec_socialni} Kč")
+col3.metric(label="Zaměstnanec zdravotní", value=f"{zamestnanec_zdravotni} Kč")
+col4.metric(label="Daň", value=f"{dan} Kč")
 
-    st.write("### Zaměstnavatel")
-    col1, col2 = st.columns(2)
-    col1.metric(label="Zaměstnavatel sociální", value=f"{zamestnavatel_socialni} Kč")
-    col2.metric(label="Zaměstnavatel zdravotní", value=f"{zamestnavatel_zdravotni} Kč")
+st.write("### Zaměstnavatel")
+col1, col2 = st.columns(2)
+col1.metric(label="Zaměstnavatel sociální", value=f"{zamestnavatel_socialni} Kč")
+col2.metric(label="Zaměstnavatel zdravotní", value=f"{zamestnavatel_zdravotni} Kč")
 
-    data = {
-        "Popis": 
-        [
-            "Základ daně:",
-            "Pojistné zaměstnavatel:",
-            "   - z toho sociální pojištění",
-            "   - z toho zdravotní pojištění",
-            "Pojistné:",
-            "   - z toho sociální pojištění",
-            "   - z toho zdravotní pojištění",
-            "Daň celkem:",
-            "   Daň:",
-            "   Daňova sleva:",
-            "   Daňové zvýhodnění na děti:"
+data = {
+    "Popis": 
+    [
+        "Základ daně:",
+        "Pojistné zaměstnavatel:",
+        "   - z toho sociální pojištění",
+        "   - z toho zdravotní pojištění",
+        "Pojistné:",
+        "   - z toho sociální pojištění",
+        "   - z toho zdravotní pojištění",
+        "Daň celkem:",
+        "   Daň:",
+        "   Daňova sleva:",
+        "   Daňové zvýhodnění na děti:"
 
 
-        ],
-        "Částka":
-        [
-            hruba_mzda,
-            -(zamestnavatel_zdravotni + zamestnavatel_socialni),
-            -zamestnavatel_socialni,
-            -zamestnavatel_zdravotni,
-            -(zamestnanec_zdravotni+zamestnanec_socialni),
-            -zamestnanec_socialni,
-            -zamestnanec_zdravotni,
-            -(dan-dan_sleva-deti_sleva),
-            -dan,
-            dan_sleva,
-            deti_sleva
-        ]
+    ],
+    "Částka":
+    [
+        hruba_mzda,
+        -(zamestnavatel_zdravotni + zamestnavatel_socialni),
+        -zamestnavatel_socialni,
+        -zamestnavatel_zdravotni,
+        -(zamestnanec_zdravotni+zamestnanec_socialni),
+        -zamestnanec_socialni,
+        -zamestnanec_zdravotni,
+        -(dan-dan_sleva-deti_sleva),
+        -dan,
+        dan_sleva,
+        deti_sleva
+    ]
+}
+
+df = pd.DataFrame(data)
+
+def highlight_alternate_rows(s):
+    return ['background-color: rgb(243, 243, 243)' if i % 2 == 0 else '' for i in range(len(s))]
+
+styled_df = df.style.apply(highlight_alternate_rows, axis=0)
+
+html_table = styled_df.to_html(index=False, escape=False)
+
+custom_css = """
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
     }
-
-    df = pd.DataFrame(data)
-
-    def highlight_alternate_rows(s):
-        return ['background-color: rgb(243, 243, 243)' if i % 2 == 0 else '' for i in range(len(s))]
-
-    styled_df = df.style.apply(highlight_alternate_rows, axis=0)
-
-    html_table = styled_df.to_html(index=False, escape=False)
-
-    custom_css = """
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-        th, td {
-            border: none; 
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        tr {
+    th, td {
         border: none; 
+        padding: 8px;
+        text-align: left;
     }
-    </style>
-    """
+    th {
+        background-color: #f2f2f2;
+    }
+    tr {
+    border: none; 
+}
+</style>
+"""
 
-    st.markdown(custom_css, unsafe_allow_html=True)
-    st.markdown(html_table, unsafe_allow_html=True)
+st.markdown(custom_css, unsafe_allow_html=True)
+st.markdown(html_table, unsafe_allow_html=True)
